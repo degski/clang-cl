5ce5054a4a3271ae10667ae1ab5535b0eb47b417
degski <degski@gmail.com>
2018-11-05 14:30:08 +0200

    Minor changes

--- main.cpp
+++ main.cpp
@@ -25,27 +25,21 @@
 #include <string>
 #include "short_alloc.h" // https://howardhinnant.github.io/stack_alloc.html
 
-using stack_string_allocator = short_alloc<char, 2048u, alignof ( char )>;
+using stack_string_allocator = short_alloc<char, 2048u, alignof ( std::max_align_t )>;
 using stack_string = std::basic_string<char, std::char_traits<char>, stack_string_allocator>;
 
 
-int main ( int argc, const char* argv [ ] ) {
+int main ( const int argc, const char* argv [ ] ) {
 
     stack_string::allocator_type::arena_type allocator;
     stack_string command { allocator };
 
-    command += "\"C:\\Program Files\\LLVM\\bin\\clang-cl.exe\" -fuse-ld=lld -flto=thin ";
+    command += "\"C:\\Program Files\\LLVM\\bin\\clang-cl.exe\" -fuse-ld=lld -flto=thin";
 
-    --argc;
-
-    int i = 1;
-
-    for ( ; i < argc; ++i ) {
-        command += argv [ i ];
+    for ( int i = 1; i < argc; ++i ) {
         command += ' ';
+        command += argv [ i ];
     }
-
-    command += argv [ i ];
 
     return std::system ( command.c_str ( ) );
 }
